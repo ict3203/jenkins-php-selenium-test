c672bbb707010bfdb2a71836d5c3459a8c0a4f96
chernhao <leechernhao123@gmail.com>
2023-11-11 14:46:18 +0800

    sds

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,10 +2,12 @@
 
 set -x
 #docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-#docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\User\\Desktop\\Docker\\github-labs\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html
 
+
+#docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html
+
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 
 sleep 1
 set +x
