f4f45967bc2bb68a648eb2da90505e11cc9b5da9
chernhao <leechernhao123@gmail.com>
2023-11-11 13:52:00 +0800

    asasa

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,7 +4,7 @@ set -x
 #docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 #docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\User\\Desktop\\Docker\\github-labs\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 
-docker run -d -p 80:80 --name jenkins-php-selenium-test -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src:/var/www/html
 
 
 sleep 1
